f87104b7f5fda88cc8db1af6aec9d54b66461ff5
DaDongDong <2795951458@qq.com>
2024-04-03 15:13:48 +0800

    Initial commit

---
+++ src/main/java/com/sks/secondkillstore/SecondKillStoreApplication.java
@@ -0,0 +1,13 @@
+package com.sks.secondkillstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondKillStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondKillStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sks/secondkillstore/SecondKillStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sks.secondkillstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondKillStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
